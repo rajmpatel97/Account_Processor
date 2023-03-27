60034cd30ec927a6a113973af619ba41b82c41dc
Raj Patel <rajmpatel97@gmail.com>
2023-03-27 16:51:59 -0400

    Init Commit

---
+++ src/main/java/com/accountprocessor/AccountProcessor/AccountProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.accountprocessor.AccountProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accountprocessor/AccountProcessor/AccountProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountprocessor.AccountProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
